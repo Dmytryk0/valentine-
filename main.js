a84b67b8df6c62e8f3d222f1328d04ff57a30e87
Dmytro Krainyk <123164804+Dmytryk0@users.noreply.github.com>
2026-02-26 18:42:54 +0100

    "NO" button delete

--- main.js
+++ main.js
@@ -2,43 +2,8 @@
 const { useState, useEffect } = React;
 
 function Page() {
-  const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
-  const [noPosition, setNoPosition] = useState({ top: "50%", left: "50%" });
   const [themeColor, setThemeColor] = useState("#ffc0cb");
-
-  const handleNoClick = () => {
-    setNoCount((prevCount) => prevCount + 1);
-    if (noCount < 2) {
-      moveButtonRandomly();
-    } else {
-      attachNoButtonToCursor();
-    }
-  };
-
-  const moveButtonRandomly = () => {
-    const randomTop = Math.random() * 80 + 10;
-    const randomLeft = Math.random() * 80 + 10;
-    setNoPosition({ top: `${randomTop}%`, left: `${randomLeft}%` });
-  };
-
-  const attachNoButtonToCursor = () => {
-    document.body.addEventListener("mousemove", (event) => {
-      setNoPosition({ top: `${event.clientY}px`, left: `${event.clientX}px` });
-    });
-  };
-
-  const getNoButtonText = () => {
-    const phrases = [
-      "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "You're breaking my heart ;(",
-    ];
-    return phrases[Math.min(noCount, phrases.length - 1)];
-  };
 
   return (
     React.createElement("div", { className: "flex flex-col items-center justify-center h-screen", style: { backgroundColor: themeColor } },
@@ -55,12 +20,7 @@ function Page() {
             React.createElement("button", {
               className: `bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4`,
               onClick: () => setYesPressed(true)
-            }, "Yes"),
-            React.createElement("button", {
-              onClick: handleNoClick,
-              className: "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded",
-              style: { position: "absolute", top: noPosition.top, left: noPosition.left }
-            }, getNoButtonText())
+            }, "Yes")
           ),
           React.createElement("div", { className: "mt-6" },
             React.createElement("label", { className: "mr-2 font-bold" }, "Choose Background Color: "),
